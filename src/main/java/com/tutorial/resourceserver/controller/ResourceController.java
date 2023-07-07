c0d8628d38819fe14a815e678878401c9e9bcaca
Cristian Blandon <cblandon@playtechla.com>
2023-07-07 15:27:35 -0500

    implementacion de servidor de recursos

---
+++ src/main/java/com/tutorial/resourceserver/controller/ResourceController.java
@@ -0,0 +1,27 @@
+package com.tutorial.resourceserver.controller;
+
+import com.tutorial.resourceserver.dto.MessageDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resource")
+public class ResourceController {
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','OIDC_USER')")
+    public ResponseEntity<MessageDto> user(Authentication authentication) {
+        return ResponseEntity.ok(new MessageDto("hello " + authentication.getName()));
+
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<MessageDto> admin(Authentication authentication) {
+        return ResponseEntity.ok(new MessageDto("hello Admin" + authentication.getName()));
+    }
+}
